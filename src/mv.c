edf0595a45552aa31cc907cd3fc4faa56ec99153
Jithin Renji <jithin0904@gmail.com>
2020-06-17 17:54:01 +0400

    mv: Source files added

---
+++ src/mv.c
@@ -0,0 +1,129 @@
+/* mv: Move (rename) files */
+
+/*
+ *      Copyright (C) 2020 Jithin Renji
+ *
+ *      This program is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <getopt.h>
+
+#include "mv.h"
+
+char progname[256] = "";
+
+struct option long_opts[] = {
+        {"verbose",     no_argument, 0, 'v'},
+        {"help",        no_argument, 0, 'h'},
+        {"version",     no_argument, 0, 'V'},
+        {0,             0,           0,  0}
+};
+
+void help (void);
+void version (void);
+
+int main (int argc, char **argv)
+{
+        strcpy(progname, argv[0]);
+
+        if (argc < 3) {
+                fprintf(stderr, "%s: Too few arguments\n", progname);
+                fprintf(stderr, "Try `%s --help` for more information.\n", progname);
+                exit(EXIT_FAILURE);
+        }
+
+        int flags = 0;
+        int opt = 0;
+        while ((opt = getopt_long(argc, argv, "vhV", long_opts, NULL)) != -1) {
+                switch (opt) {
+                case 'v':
+                        flags |= MV_VERBOSE;
+                        break;
+                case 'h':
+                        help();
+                        exit(EXIT_FAILURE);
+                        break;
+
+                case 'V':
+                        version();
+                        exit(EXIT_FAILURE);
+                        break;
+
+                default:
+                        fprintf(stderr, "Try `%s --help` for more information\n", progname);
+                        exit(EXIT_FAILURE);
+                        break;
+                }
+        }
+
+        if (argv[optind] == NULL || argc < 3) {
+                fprintf(stderr, "%s: Missing operand\n", progname);
+                fprintf(stderr, "Try `%s --help` for more information.\n", progname);
+                exit(EXIT_FAILURE);
+        }
+
+        /* The command is in the form mv [OPTION]... FILE1 FILE2 */
+        if (argc - optind == 2) {
+                int fd = open(argv[optind], O_RDONLY);
+
+                if (fd == -1) {
+                        fprintf(stderr, "%s: `%s`: ", progname, argv[optind]);
+                        perror("");
+                        close(fd);
+
+                        exit(EXIT_FAILURE);
+                }
+
+                close(fd);
+
+                int ret = rename(argv[optind], argv[optind + 1]);
+                if (ret == -1) {
+                        fprintf(stderr, "%s: ", progname);
+                        perror("");
+                        exit(EXIT_FAILURE);
+                }
+
+                if (CHKF_MV_VERBOSE(flags))
+                        printf("Renamed `%s` to `%s`\n", argv[optind], argv[optind + 1]);
+        }
+
+        return 0;
+}
+
+void help (void)
+{
+        printf("Usage: %s [OPTION]... DIRECTORY...\n\n", progname);
+
+        printf("Options:\n"
+               "\t-h, --help\tShow this help message and exit\n"
+               "\t-V, --version\tShow version information and exit\n");
+}
+
+void version (void)
+{
+        printf("%s (ClaireUTILS) v0.1\n", progname);
+
+        printf("Copyright (C) 2020 Jithin Renji\n"
+"License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.\n"
+"This is free software: you are free to change and redistribute it.\n"
+"There is NO WARRANTY, to the extent permitted by law.\n\n"
+"Written by Jithin Renji.\n");
+}
---
+++ src/mv.h
@@ -0,0 +1,30 @@
+/* mv.h: Definitions for the `mv` program. */
+
+/*
+ *      Copyright (C) 2020 Jithin Renji
+ *
+ *      This program is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * */
+
+#ifndef MV_H
+#define MV_H
+
+/* Flags */
+#define MV_VERBOSE              1
+
+/* Check flag macros */
+#define CHKF_MV_VERBOSE(flags) ((flags & MV_VERBOSE) == MV_VERBOSE)
+
+#endif	/* PROG_H */
